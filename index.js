70546ecab8d62505bb8192c70b68ce8afa1d83b9
rZoysa <rithikzoysa@gmail.com>
2024-03-24 23:33:56 +0530

    update

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const cors = require("cors");
 const apiUrl = "https://autopilot97-8dad7d99b556.herokuapp.com";
 
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: 'https://se-97-seller-side.vercel.app'
+}));
+
 
 
 
