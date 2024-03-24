eedd50df2ec36cf78b17009b6451e3f9d0853ee5
rZoysa <rithikzoysa@gmail.com>
2024-03-25 01:14:33 +0530

    Update

--- index.js
+++ index.js
@@ -66,7 +66,6 @@ app.post("/upload", upload.single("product"), async (req, res) => {
   });
 });
 
-
 app.get("/images/:imageId", (req, res) => {
   const imageId = req.params.imageId;
   gfs.files.findOne({ _id: mongoose.Types.ObjectId(imageId) }, (err, file) => {
