195bab1e7a3038fa48aa06942953d69d1da303d2
rZoysa <rithikzoysa@gmail.com>
2024-03-24 23:29:12 +0530

    update

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
 
+const apiUrl = "https://autopilot97-8dad7d99b556.herokuapp.com";
+
 app.use(express.json());
 app.use(cors());
 
@@ -34,7 +36,7 @@ const upload = multer({ storage: storage });
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `/images/${req.file.filename}`,
+    image_url: apiUrl + `/images/${req.file.filename}`,
   });
 });
 app.use("/images", express.static("upload/images"));
